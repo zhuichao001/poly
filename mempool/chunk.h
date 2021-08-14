1a030117ab52635071ebad2344929d15bcc58824
zhuichao001 <zhuichao001@163.com>
2021-08-14 19:53:20 +0800

    cancel mempool

--- mempool/chunk.h
+++
@@ -1,5 +0,0 @@
-#ifndef _POLY_MALLOC_H_
-#define _POLY_MALLOC_H_
-
-
-#endif
--- mempool/tiny.h
+++
@@ -1,9 +0,0 @@
-namespace poly{
-
-//2^4
-struct piece{
-    void *prev;
-    void *next;
-};
-
-}//end of namespace poly
